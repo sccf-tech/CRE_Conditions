d4761f1a0dfd6891710f679a0c4996cdb7a45f2a
Paul Julian <36565183+SwampThingPaul@users.noreply.github.com>
2021-10-12 10:23:43 -0400

    edits

--- src/RT_BG_HABS data_v2.R
+++ src/RT_BG_HABS data_v2.R
@@ -119,21 +119,21 @@ path <- 'https://services1.arcgis.com/nRHtyn3uE1kyzoYc/arcgis/rest/services/VIEW
 request <- GET(
   url = path,
   query= list(       
-    where = "County='Lee' OR County='Collier' OR County='Hendry' OR County='Glades'",
+    where = "County='Lee' OR County='Collier' OR County='Hendry' OR County='Glades' OR County='Martin'",
     outFields = '*',
     f = 'pjson'
   )
 )
 response <- content(request, as = "text", encoding = "UTF-8")
 results <- jsonlite::fromJSON(response,flatten=T)
-nrow(results$features)
+# nrow(results$features)
 
 bgdat=results$features
 
 request <- GET(
   url = path,
   query= list(       
-    where = "County='Okeechobee' OR County='PalmBeach' OR County='Broward' OR County='MiamiDade'",
+    where = "County='Okeechobee' OR County='PalmBeach' OR County='Broward' OR County='MiamiDade' OR County='Monroe'",
     outFields = '*',
     f = 'pjson'
   )
@@ -162,6 +162,25 @@ bgdat$datetime=as.POSIXct(bgdat$sitevisitdate,origin = c('1970-01-01'), tz = 'UT
 bgdat$datetime=date.fun(bgdat$datetime,form="%F %R")
 bgdat$date=date.fun(bgdat$datetime)
 
+unique(bgdat$othertoxin)
+vars=strsplit(bgdat$othertoxin,"\\;|\\:")
+othertox=data.frame(tox1=sapply(vars,"[",1),tox1.rslt=sapply(vars,"[",2),
+           tox2=sapply(vars,"[",3),tox2.rslt=sapply(vars,"[",4),
+           tox3=sapply(vars,"[",5),tox3.rslt=sapply(vars,"[",6),
+           tox4=sapply(vars,"[",7),tox4.rslt=sapply(vars,"[",8))
+unique(othertox$tox1)
+unique(othertox$tox2)
+unique(othertox$tox3)
+
+othertox.mdl=data.frame(tox=c("Anatoxin-a","Cylindrospermopsin"),
+                        MDL=c(0.25))
+
+
+vars=strsplit(bgdat$microcystin,"I")
+# bgdat$microcystin.halfmdl=ifelse(sapply(vars,"[",1)=="not detected",0.5,as.numeric(sapply(vars,"[",1)))
+bgdat$microcystin.val=ifelse(sapply(vars,"[",1)=="not detected",NA,as.numeric(sapply(vars,"[",1)))
+bgdat[,c("microcystin","microcystin.halfmdl")]
+
 bgdat_30d=subset(bgdat,date<TODAY&date>sdate)
 bgdat_30d.shp=SpatialPointsDataFrame(coords=bgdat[,c("longitude","latitude")],
                                               data=bgdat,
@@ -178,9 +197,103 @@ tm_shape(bgdat_30d.shp)+tm_dots(col="toxinpresent",
                                              "algalid","othertoxin"),
                                 group="FDEP Blue-green Algae Data",
                                 title="Cyanobacteria dominate")+
-  tm_layout("Blue-Green Algae")
-
+  tm_shape(bgdat_30d.shp)+tm_bubbles(col="white",size="microcystin.val", id="date",group="Microcystin conc.")+
+  tm_shape(bgdat.ext)+tm_borders(group="Area of Interest",lty="dashed",col="grey",lwd=1)
 
 sum(bgdat_30d$cyanobacteriadominant=="Yes")
 sum(bgdat_30d$toxinpresent=="Yes")
 range(as.numeric(bgdat_30d$microcystin),na.rm = T)
+
+
+## Species heat map idea
+bgdat_30d$algalid
+bgdat_30d$algaeobserved
+bgdat_30d[,c("algalid","algaeobserved")]
+
+
+
+bgdat_30d.algae=strsplit(bgdat_30d$algalid,"\\;|\\:| and")
+
+algae.list=rbind(
+cbind(bgdat_30d[,c("objectid","date")],data.frame(val=sapply(bgdat_30d.algae,"[",1))),
+cbind(bgdat_30d[,c("objectid","date")],data.frame(val=sapply(bgdat_30d.algae,"[",2))),
+cbind(bgdat_30d[,c("objectid","date")],data.frame(val=sapply(bgdat_30d.algae,"[",3)))
+)
+
+unique(algae.list$val)
+
+algae.list=subset(algae.list,!(val%in%c("Dominant taxon",
+                                  "Co-dominate taxa were","Co-dominant taxa were"
+                                  ,NA,"no dominant species in sample",
+                                  " no dominant species in sample")))
+
+
+algae.list.plot=data.frame(val=c("mixed algae", " Microcystis aeruginosa", " Cylindrospermopsis raciborskii", 
+                                 " Planktolyngbya limnetica", " Euglena sp."),
+                           plot.y=1:5)
+fill=expand.grid(date=date.fun(seq(min(algae.list$date),max(algae.list$date),"1 days")),
+            val=algae.list.plot$val)
+
+algae.list=merge(algae.list,fill,c("date","val"),all.y=T)
+algae.list.sum=reshape::cast(algae.list,date~val,value="objectid",fun.aggregate=function(x) N.obs(x))
+
+heatmap(as.matrix(algae.list.sum),Colv = NA, Rowv = NA, scale="column")
+
+algae.list.sum=algae.list$val=as.factor(algae.list$val)
+
+
+
+
+algae.list.sum=ddply(algae.list,c("date","val"),summarise,N.val=N.obs(objectid))
+algae.list.sum=merge(algae.list.sum,fill,c("date","val"),all.y=T)
+algae.list.sum=merge(algae.list.sum,algae.list.plot,"val",all.y=T)
+algae.list.sum$N.val=with(algae.list.sum,ifelse(is.na(N.val)==T,0,N.val))
+
+algae.list.sum$val.cat=as.factor(findInterval(algae.list.sum$val,c(0,1,3,5,10,15)))
+algae.list.sum$date2=as.numeric(algae.list.sum$date)
+cols.vir=c("grey80",rev(viridis::inferno(5)))
+algae.list.sum=merge(algae.list.sum,
+                     data.frame(val.cat=c(1:6),
+                                val.cat.txt=c(0,1,"1 - 3","3 - 5","5 - 10","10 -15")),"val.cat")
+
+plot(plot.y~date,algae.list.sum)
+
+cols=c("1"=cols.vir[1],"2"=cols.vir[2],"3"=cols.vir[3],"4"=cols.vir[4],"5"=cols.vir[5],"6"=cols.vir[6])
+library(lubridate)
+x.seq.rect=date.fun(seq(min(algae.list.sum$date)-duration(0.5,"days"),
+               max(algae.list.sum$date)+duration(0.5,"days"),"1 days"))
+plot(plot.y~date,algae.list.sum)
+for(i in 1:nrow(algae.list.plot)){
+  tmp=subset(algae.list.sum,val==algae.list.plot$val[i])
+  rect(x.seq.rect,i+0.5,max(x.seq.rect),i-0.5,col=cols[tmp$val.cat])
+}
+
+
+library(ggplot2)
+library(cowplot)
+cols.wes=rev(wesanderson::wes_palette("Zissou1",5,"continuous"))
+cols.vir=rev(viridis::inferno(5))
+cols=c("1"=cols.vir[1],"2"=cols.vir[2],"3"=cols.vir[3],"4"=cols.vir[4],"5"=cols.vir[5])
+ggplot(algae.list.sum, aes(x = date2, y = plot.y, fill = val.cat)) +
+  geom_tile(aes(group = val.cat), colour = 'black')+
+  #geom_text(aes(label=format(round(TRF.in,1),nsmall=1),fontface = "bold"),size=2,colour=rf.dat.mon$txt.cols,family="serif",)+
+  scale_y_reverse(expand = c(0, 0), breaks = algae.list.sum$date2) +
+  # scale_x_discrete(expand = c(0, 0), position = 'top') +
+  # scale_x_continuous(expand = c(0, 0), breaks = 1:12)+
+  scale_fill_manual(values = cols,
+                    name="Sample Counts",
+                    breaks=1:5,
+                    labels=c(1, 3 ,5, 10, 15)) +
+  theme_bw() +
+  theme(
+    #legend.position = 'none',
+    text=element_text(family="serif"),
+    plot.title=element_text(size=12),
+    plot.subtitle = element_text(color = "grey50",size=8),
+    plot.caption = element_text(hjust = 0)
+  )+
+  labs(title = "Southwest Florida Rainfall",
+       subtitle = "Total Monthly Rainfall",
+       caption = paste0("Produced: ",format(Sys.Date(),"%d %b %Y")),
+       x="Month",
+       y="Year")
